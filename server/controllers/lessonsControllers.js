2602a15b7c4daa08de6a6ef2cac851bcdee40073
douglas86 <douglasmaxton@gmail.com>
2022-08-01 20:19:00 +0200

    Create two get requests by id and one gets all requests

--- server/controllers/lessonsControllers.js
+++ server/controllers/lessonsControllers.js
@@ -3,6 +3,22 @@ import { pool } from "../lib/pool.js";
 import { thenService } from "../services/thenService.js";
 import { catchService } from "../services/catchService.js";
 
+export const get = (req, res) => {
+  pool
+    .query(`SELECT * FROM lessons ORDER BY id`)
+    .then((result) => res.status(200).json(result.rows))
+    .catch((err) => catchService(res, err));
+};
+
+export const getById = (req, res) => {
+  const id = req.params.lessonId;
+  const query = `SELECT * FROM lessons WHERE id = $1`;
+  pool
+    .query(query, [id])
+    .then((result) => res.status(200).json(result.rows))
+    .catch((err) => catchService(res, err));
+};
+
 export const post = (req, res) => {
   const { title, img_url, intro, summary, content, video_url } = req.body;
   const query = `INSERT INTO lessons (title, img_url, intro, summary, content, video_url) VALUES ($1,$2,$3,$4,$5,$6)`;
@@ -20,4 +36,17 @@ export const put = (req, res) => {
     .query(query, [title, img_url, intro, summary, content, video_url, id])
     .then(() => thenService(res, "Lesson Updated"))
     .catch((err) => catchService(res, err));
+};
+
+export const del = (req, res) => {
+  const id = req.params.lessonId;
+  const query = `SELECT * FROM lessons WHERE id = $1`;
+  pool.query(query, [id]).then((result) => {
+    if (result.rows.length > 0) {
+      pool
+        .query(`DELETE FROM lessons WHERE id=$1`, [id])
+        .then(() => thenService(res, "Lesson Deleted"))
+        .catch((err) => catchService(res, err));
+    }
+  });
 };
--- server/routes/lessonsRoutes.js
+++ server/routes/lessonsRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { post, put } from "../controllers/lessonsControllers.js";
+import { del, get, post, put } from "../controllers/lessonsControllers.js";
 
 const router = express.Router();
 
+router.get("/", (req, res) => get(req, res));
+router.get("/:lessonId", (req, res) => getById(req, res));
 router.post("/", (req, res) => post(req, res));
 router.put("/:lessonId", (req, res) => put(req, res));
+router.delete("/:lessonId", (req, res) => del(req, res));
 
 export default router;
