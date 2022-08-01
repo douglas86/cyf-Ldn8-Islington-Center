04e1be38f14e4e6259bb10a8800ae6d5dcb1d1c6
douglas86 <douglasmaxton@gmail.com>
2022-08-01 22:44:40 +0200

    Finished creating all question endpoints

---
+++ server/routes/questionsRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { del, get, post } from "../controllers/questionsControllers.js";
+
+const router = express.Router();
+
+router.post("/", (req, res) => post(req, res));
+router.get("/lessons/:lessonId", (req, res) => get(req, res));
+router.put("/", (req, res) => put(req, res));
+router.delete("/:questionId", (req, res) => del(req, res));
+
+export default router;
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 
 import homeRoutes from "./routes/homeRoutes.js";
 import lessonsRoutes from "./routes/lessonsRoutes.js";
+import questionsRoutes from "./routes/questionsRoutes.js";
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(cors());
 
 app.use("/", homeRoutes);
 app.use("/lessons", lessonsRoutes);
+app.use("/questions", questionsRoutes);
 
 const PORT = process.env.PORT || 5000;
 
