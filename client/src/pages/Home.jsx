fd56daffc347af959e9cfd3c3d82e1e87944e5fe
douglas86 <douglasmaxton@gmail.com>
2022-08-02 00:29:53 +0200

    Added the background img with text on top of image

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,18 @@
+import styles from "./styles.module.css";
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <div className={styles.showcase}>
+        <div className={styles.container}>
+          <h1>Islington Center for Refugees and Migrants</h1>
+          <p className={`${styles.lead} ${styles.hide_on_small}`}>
+            Welcoming and supporting Refugees, Asylum seekers, and migrants in
+            our community
+          </p>
+        </div>
+      </div>
+    </>
   );
 };
 
